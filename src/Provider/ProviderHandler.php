5003393ead42f208bacef04b3556f812103c92c9
Justin Hoffman <justin@ohmedia.ca>
2022-03-08 10:32:49 -0600

    camel case

--- src/Provider/ProviderHandler.php
+++ src/Provider/ProviderHandler.php
@@ -5,27 +5,27 @@ namespace OHMedia\SecurityBundle\Provider;
 class ProviderHandler
 {
     private $providers;
-    
+
     public function __construct(iterable $providers)
     {
         $this->providers = $providers;
     }
-    
+
     public function getEntityActions()
     {
-        $entity_actions = [];
-        
+        $entityActions = [];
+
         foreach ($this->providers as $provider) {
-            $entity_actions[] = [
+            $entityActions[] = [
                 'name' => $provider->getEntityName(),
                 'actions' => $provider->getEntityActions()
             ];
         }
-        
-        usort($entity_actions, function($a, $b) {
+
+        usort($entityActions, function($a, $b) {
             return $a['name'] <=> $b['name'];
         });
-        
-        return $entity_actions;
+
+        return $entityActions;
     }
 }
