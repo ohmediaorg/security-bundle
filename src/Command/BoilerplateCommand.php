2a19d14987a78a496b5358e90b9a40b6865ba79d
Justin Hoffman <justin@ohmedia.ca>
2022-03-08 15:33:53 -0600

    move the templates and simplify to use basic find/replace templates

--- src/Command/BoilerplateCommand.php
+++ src/Command/BoilerplateCommand.php
@@ -2,26 +2,24 @@
 
 namespace OHMedia\SecurityBundle\Command;
 
-use OHMedia\SecurityBundle\Interfaces\CleanerInterface;
-use Symfony\Bundle\MakerBundle\Generator;
-use Symfony\Bundle\MakerBundle\InputConfiguration;
-use Symfony\Bundle\MakerBundle\Str;
-use Symfony\Bundle\MakerBundle\Util\ClassNameDetails;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
+use function Symfony\Component\String\u;
+
 class BoilerplateCommand extends Command
 {
-    private $generator;
+    private $projectDir;
 
-    public function __construct(Generator $generator)
+    public function __construct(string $projectDir)
     {
-        $this->generator = $generator;
-        $this->inputConfig = new InputConfiguration();
+        $this->projectDir = $projectDir;
+        $this->templateDir = __DIR__ . '/../../boilerplate/';
+        $this->find = [];
+        $this->replace = [];
 
         parent::__construct();
     }
@@ -31,6 +29,7 @@ class BoilerplateCommand extends Command
         $this
             ->setName('ohmedia:security:boilerplate')
             ->setDescription('Command to create the files needed for an entity')
+            ->addOption('user', null, InputOption::VALUE_NONE, 'Specify that this is the user entity')
         ;
     }
 
@@ -38,10 +37,7 @@ class BoilerplateCommand extends Command
     {
         $this->io = new SymfonyStyle($input, $output);
 
-        $className = $this->io->ask(sprintf(
-            'Class name of the entity to create (e.g. <fg=yellow>%s</>)',
-            Str::asClassName(Str::getRandomTerm())
-        ));
+        $className = $this->io->ask('Class name of the entity to create');
 
         if (!$className) {
             $this->io->error('Please provide the class name');
@@ -49,45 +45,63 @@ class BoilerplateCommand extends Command
             return Command::INVALID;
         }
 
-        $lockable = $this->io->confirm('Add Lockable functionality?');
+        if ($input->getOption('user')) {
+            $extend = 'EntityUser';
+            $extendUse = 'OHMedia\SecurityBundle\Entity\User as EntityUser';
+        }
+        else {
+            $extend = 'Entity';
+            $extendUse = 'OHMedia\SecurityBundle\Entity\Entity';
+        }
 
-        $entityDetails = $this->generator->createClassNameDetails(
-            $className,
-            'Entity'
-        );
+        $camelCase = u($className)->camel();
+        $snakeCase = u($className)->snake();
+        $pascalCase = u($camelCase)->title();
+        $readable = u($snakeCase)->replace('_', ' ');
 
-        $repositoryDetails = $this->generator->createClassNameDetails(
-            $className,
-            'Repository',
-            'Repository'
-        );
+        $findReplace = [
+            '__ENTITYEXTEND' => $extend,
+            '__ENTITYEXTENDUSE' => $extendUse,
+            '__CAMELCASE__' => $camelCase,
+            '__SNAKECASE__' => $snakeCase,
+            '__PASCALCASE__' => $pascalCase,
+            '__READABLE__' => $readable,
+        ];
 
-        $formDetails = $this->generator->createClassNameDetails(
-            $className,
-            'Form',
-            'Type'
-        );
+        $this->find = array_keys($findReplace);
+        $this->replace = array_values($findReplace);
 
-        $providerDetails = $this->generator->createClassNameDetails(
-            $className,
-            'Provider',
-            'Provider'
-        );
+        $entityFile = sprintf('src/Entity/%s.php', $pascalCase);
+        $repositoryFile = sprintf('src/Repository/%sRepository.php', $pascalCase);
+        $formFile = sprintf('src/Form/%sType.php', $pascalCase);
+        $providerFile = sprintf('src/Provider/%sProvider.php', $pascalCase);
+        $controllerFile = sprintf('src/Controller/%sController.php', $pascalCase);
+        $routesFile = sprintf('config/routes/%s.yaml', $snakeCase);
+        $voterFile = sprintf('src/Security/Voter/%sVoter.php', $pascalCase);
 
-        $controllerDetails = $this->generator->createClassNameDetails(
-            $className,
-            'Controller',
-            'Controller'
-        );
-
-        $voterDetails = $this->generator->createClassNameDetails(
-            $className,
-            'Security\Voter',
-            'Voter'
-        );
-
-        // routing
+        $this
+            ->generateFile('Entity.php.tpl', $entityFile)
+            ->generateFile('Repository.php.tpl', $repositoryFile)
+            ->generateFile('Form.php.tpl', $formFile)
+            ->generateFile('Provider.php.tpl', $providerFile)
+            ->generateFile('Controller.php.tpl', $controllerFile)
+            ->generateFile('routes.yaml.tpl', $routesFile)
+            ->generateFile('Voter.php.tpl', $voterFile)
+        ;
 
         return Command::SUCCESS;
+    }
+
+    private function generateFile(string $template, string $destination)
+    {
+        $contents = str_replace(
+            $this->find,
+            $this->replace,
+            file_get_contents($this->templateDir . $template)
+        );
+
+        file_put_contents($this->projectDir . $destination, $contents);
+
+        return $this;
     }
 }
