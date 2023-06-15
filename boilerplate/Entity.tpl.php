e8d5200c68e1537f46ce6f39ffddbdd9fabed38f
Justin Hoffman <justin@ohmedia.ca>
2023-06-15 16:28:58 -0600

    update boilerplate to not use entity class

--- boilerplate/Entity.tpl.php
+++ boilerplate/Entity.tpl.php
@@ -4,17 +4,27 @@ namespace App\Entity;
 
 use App\Repository\<?= $singular['pascal_case'] ?>Repository;
 use Doctrine\ORM\Mapping as ORM;
+use OHMedia\SecurityBundle\Entity\Traits\Blameable;
 <?php if ($is_user) { ?>
 use OHMedia\SecurityBundle\Entity\User as EntityUser;
-<?php } else { ?>
-use OHMedia\SecurityBundle\Entity\Entity;
 <?php } ?>
 
 #[ORM\Entity(repositoryClass: <?= $singular['pascal_case'] ?>Repository::class)]
 <?php if ($is_user) { ?>
 class <?= $singular['pascal_case'] ?> extends EntityUser
 <?php } else { ?>
-class <?= $singular['pascal_case'] ?> extends Entity
+class <?= $singular['pascal_case'] ?>
 <?php } ?>
 {
+    use Blameable;
+
+    #[ORM\Id()]
+    #[ORM\GeneratedValue()]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
 }
