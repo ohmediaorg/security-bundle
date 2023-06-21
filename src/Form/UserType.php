1c3e4cce78e6c048270eb53b4d4cfa535b0f792b
Justin Hoffman <justin@ohmedia.ca>
2023-06-21 15:23:56 -0600

    implement user form

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,6 +4,10 @@ namespace OHMedia\SecurityBundle\Form;
 
 use OHMedia\SecurityBundle\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -11,9 +15,28 @@ class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $user = isset($options['data']) ? $options['data'] : null;
+
+        // need to get logged-in user
+        // developer user cannot be disabled
+
         $builder
-            // TODO: add actual fields
+            ->add('email', EmailType::class)
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'options' => ['attr' => ['autocomplete' => 'new-password']],
+                'invalid_message' => 'The password fields must match.',
+                'first_options'  => ['label' => 'New Password'],
+                'second_options' => ['label' => 'Repeat Password'],
+                'mapped' => false,
+            ])
         ;
+
+        if ($user->isDeveloper()) {
+            $builder->add('enabled', CheckboxType::class, [
+                'required' => true,
+            ]);
+        }
     }
 
     public function configureOptions(OptionsResolver $resolver)
