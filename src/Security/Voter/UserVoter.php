f2ce53a4d3954fbdbc593e0a5ea11f5e87b46950
Justin Hoffman <justin@ohmedia.ca>
2023-06-21 14:08:22 -0600

    sensible defaults for user voter

---
+++ src/Security/Voter/UserVoter.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace OHMedia\SecurityBundle\Security\Voter;
+
+use OHMedia\SecurityBundle\Entity\User;
+use OHMedia\SecurityBundle\Security\Voter\EntityVoter;
+
+class UserVoter extends EntityVoter
+{
+    const ATTRIBUTE_PREFIX = 'user_';
+    const INDEX = self::ATTRIBUTE_PREFIX . 'index';
+    const CREATE = self::ATTRIBUTE_PREFIX . 'create';
+    const EDIT = self::ATTRIBUTE_PREFIX . 'edit';
+    const DELETE = self::ATTRIBUTE_PREFIX . 'delete';
+
+    protected function getEntityClass(): string
+    {
+        return User::class;
+    }
+
+    protected function canIndex(User $user, User $loggedIn): bool
+    {
+        return true;
+    }
+
+    protected function canCreate(User $user, User $loggedIn): bool
+    {
+        return true;
+    }
+
+    protected function canEdit(User $user, User $loggedIn): bool
+    {
+        return true;
+    }
+
+    protected function canDelete(User $user, User $loggedIn): bool
+    {
+        return $user !== $loggedIn;
+    }
+}
