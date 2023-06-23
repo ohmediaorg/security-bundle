2a01700c2aecb38d06c1744a4f1f91e792a5fab6
Justin Hoffman <justin@ohmedia.ca>
2023-06-23 13:40:40 -0600

    rename and make sure create user command works

--- src/Command/CreateUserCommand.php
+++ src/Command/CreateUserCommand.php
@@ -11,7 +11,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
-class UserCreateCommand extends Command
+class CreateUserCommand extends Command
 {
     private $passwordHasher;
     private $userRepository;
