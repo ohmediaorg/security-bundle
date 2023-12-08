85b93ffd8d2f3157e97c7b5e0fa312e3ae9cc278
justin-oh <justin@ohmedia.ca>
2023-12-08 11:42:31 -0600

    deprecation fix

--- boilerplate/Form.tpl.php
+++ boilerplate/Form.tpl.php
@@ -9,14 +9,14 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class <?php echo $singular['pascal_case']; ?>Type extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             // add your form fields...
         ;
     }
 
-    public function configureOptions(OptionsResolver $resolver)
+    public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
             'data_class' => <?php echo $singular['pascal_case']; ?>::class,
