dcc3f5b3f0251b57e8ffc850da4a561f86e04e7a
justin-oh <justin@ohmedia.ca>
2024-04-04 10:09:45 -0600

    Create EntityChoiceManager.php

---
+++ src/Service/EntityChoiceManager.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace OHMedia\SecurityBundle\Service;
+
+class EntityChoiceManager
+{
+    private array $entityChoices = [];
+
+    public function addEntityChoice(EntityChoiceInterface $entityChoice)
+    {
+        $this->entityChoices[] = $entityChoice;
+    }
+
+    public function getEntityChoices(): array
+    {
+        return $this->entityChoices;
+    }
+
+    public function transformEntitiesToEntityChoices(string ...$entities): array
+    {
+        $entityChoices = [];
+
+        foreach ($this->entityChoices as $entityChoice) {
+            $intersect = array_intersect($entities, $entityChoice->getEntities());
+
+            if ($intersect) {
+                $entityChoices[] = $entityChoice;
+            }
+        }
+
+        return $entityChoices;
+    }
+
+    public function transformEntityChoicesToEntities(EntityChoiceInterface ...$entityChoices): array
+    {
+        $entities = [];
+
+        foreach ($entityChoices as $entityChoice) {
+            $entities = array_merge($entities, $entityChoice->getEntities());
+        }
+
+        return array_unique($entities);
+    }
+}
