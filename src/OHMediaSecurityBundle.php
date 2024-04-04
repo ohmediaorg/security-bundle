463dbcfcb8c439f1e013c864f8a32b173c4bc81d
justin-oh <justin@ohmedia.ca>
2024-04-04 10:11:41 -0600

    Update OHMediaSecurityBundle.php

--- src/OHMediaSecurityBundle.php
+++ src/OHMediaSecurityBundle.php
@@ -2,8 +2,16 @@
 
 namespace OHMedia\SecurityBundle;
 
+use OHMedia\SecurityBundle\DependencyInjection\Compiler\EntityChoicePass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class OHMediaSecurityBundle extends Bundle
 {
+    public function build(ContainerBuilder $container): void
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new EntityChoicePass());
+    }
 }
