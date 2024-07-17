b696429f8922094500745b6f841aec253a219fbb
Justin Hoffman <justin@ohmedia.ca>
2024-07-17 12:50:47 -0600

    strip profile stuff out of user controller

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,22 +4,18 @@ namespace OHMedia\SecurityBundle\Controller;
 
 use OHMedia\BackendBundle\Routing\Attribute\Admin;
 use OHMedia\BootstrapBundle\Service\Paginator;
-use OHMedia\EmailBundle\Entity\Email;
 use OHMedia\EmailBundle\Repository\EmailRepository;
-use OHMedia\EmailBundle\Util\EmailAddress;
 use OHMedia\SecurityBundle\Entity\User;
 use OHMedia\SecurityBundle\Form\UserType;
 use OHMedia\SecurityBundle\Repository\UserRepository;
 use OHMedia\SecurityBundle\Security\Voter\UserVoter;
 use OHMedia\UtilityBundle\Form\DeleteType;
-use OHMedia\UtilityBundle\Util\RandomString;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 #[Admin]
 class UserController extends AbstractController
@@ -112,25 +108,7 @@ class UserController extends AbstractController
             'user' => $user,
             'form' => $form->createView(),
             'form_title' => 'Create User',
-            'is_profile' => false,
         ]);
-    }
-
-    #[Route('/profile', name: 'user_profile', methods: ['GET', 'POST'])]
-    public function profile(
-        EmailRepository $emailRepository,
-        Request $request,
-        UserPasswordHasherInterface $passwordHasher,
-    ): Response {
-        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
-
-        return $this->editForm(
-            $emailRepository,
-            $request,
-            $this->getUser(),
-            $passwordHasher,
-            true
-        );
     }
 
     #[Route('/user/{id}/edit', name: 'user_edit', methods: ['GET', 'POST'])]
@@ -146,52 +124,16 @@ class UserController extends AbstractController
             'You cannot edit this user.'
         );
 
-        return $this->editForm(
-            $emailRepository,
-            $request,
-            $user,
-            $passwordHasher,
-            false
-        );
-    }
-
-    private function editForm(
-        EmailRepository $emailRepository,
-        Request $request,
-        User $user,
-        UserPasswordHasherInterface $passwordHasher,
-        bool $isProfile
-    ): Response {
         $form = $this->createForm(UserType::class, $user, [
             'logged_in' => $this->getUser(),
         ]);
 
         $form->add('submit', SubmitType::class);
 
-        $oldEmail = $user->getEmail();
-
         $form->handleRequest($request);
 
         if ($form->isSubmitted()) {
             if ($form->isValid()) {
-                $newEmail = $form->get('email')->getData();
-
-                $verifyEmail = $oldEmail !== $newEmail;
-
-                if ($verifyEmail) {
-                    $token = RandomString::get(50, function ($token) {
-                        return !$this->userRepository->findOneBy([
-                            'verify_token' => $token,
-                        ]);
-                    });
-
-                    $user
-                        ->setEmail($oldEmail)
-                        ->setVerifyToken($token)
-                        ->setVerifyEmail($newEmail)
-                    ;
-                }
-
                 $password = $form->get('password')->getData();
 
                 if ($password) {
@@ -205,19 +147,13 @@ class UserController extends AbstractController
 
                 $this->userRepository->save($user, true);
 
-                $noun = $isProfile ? 'your profile' : 'the user';
-
-                if ($verifyEmail) {
-                    $this->addFlash('notice', "Changes to $noun were saved successfully. The new email address will need to be verified before that change takes effect.");
-
-                    $this->createVerificationEmail($emailRepository, $user);
+                if ($user->shouldSendVerifyEmail()) {
+                    $this->addFlash('notice', 'Changes to the user were saved successfully. The new email address will need to be verified before that change takes effect.');
                 } else {
-                    $this->addFlash('notice', "Changes to $noun were saved successfully.");
+                    $this->addFlash('notice', 'Changes to the user were saved successfully.');
                 }
 
-                return $isProfile
-                    ? $this->redirectToRoute('user_profile')
-                    : $this->redirectToRoute('user_index');
+                return $this->redirectToRoute('user_index');
             }
 
             $this->addFlash('error', 'There are some errors in the form below.');
@@ -226,29 +162,8 @@ class UserController extends AbstractController
         return $this->render('@OHMediaSecurity/user/user_form.html.twig', [
             'user' => $user,
             'form' => $form->createView(),
-            'form_title' => $isProfile ? 'Profile' : 'Edit User',
-            'is_profile' => $isProfile,
+            'form_title' => 'Edit User',
         ]);
-    }
-
-    private function createVerificationEmail(EmailRepository $emailRepository, User $user)
-    {
-        $url = $this->generateUrl('user_verify_email', [
-            'token' => $user->getVerifyToken(),
-        ], UrlGeneratorInterface::ABSOLUTE_URL);
-
-        $to = new EmailAddress($user->getVerifyEmail(), $user->getFullName());
-
-        $email = (new Email())
-            ->setSubject('Verify Email Address')
-            ->setTemplate('@OHMediaSecurity/email/verification_email.html.twig', [
-                'user' => $user,
-                'url' => $url,
-            ])
-            ->setTo($to)
-        ;
-
-        $emailRepository->save($email, true);
     }
 
     #[Route('/user/{id}/delete', name: 'user_delete', methods: ['GET', 'POST'])]
