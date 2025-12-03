d65ffbc6dd6a172d2b164b26270679affdb897ed
Justin Hoffman <justin@ohmedia.ca>
2025-12-03 17:14:02 -0600

    add listing search

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,6 +2,7 @@
 
 namespace OHMedia\SecurityBundle\Controller;
 
+use Doctrine\ORM\QueryBuilder;
 use OHMedia\BackendBundle\Form\MultiSaveType;
 use OHMedia\BackendBundle\Routing\Attribute\Admin;
 use OHMedia\BootstrapBundle\Service\Paginator;
@@ -9,10 +10,14 @@ use OHMedia\SecurityBundle\Entity\User;
 use OHMedia\SecurityBundle\Form\UserType;
 use OHMedia\SecurityBundle\Repository\UserRepository;
 use OHMedia\SecurityBundle\Security\Voter\UserVoter;
+use OHMedia\SecurityBundle\Service\EntityChoiceManager;
 use OHMedia\UtilityBundle\Form\DeleteType;
 use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -21,12 +26,14 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Admin]
 class UserController extends AbstractController
 {
-    public function __construct(private UserRepository $userRepository)
-    {
+    public function __construct(
+        private EntityChoiceManager $entityChoiceManager,
+        private UserRepository $userRepository,
+    ) {
     }
 
     #[Route('/users', name: 'user_index', methods: ['GET'])]
-    public function index(Paginator $paginator): Response
+    public function index(Paginator $paginator, Request $request): Response
     {
         $this->denyAccessUnlessGranted(
             UserVoter::INDEX,
@@ -54,6 +61,10 @@ class UserController extends AbstractController
 
         $qb->orderBy('ord', 'ASC');
 
+        $searchForm = $this->getSearchForm($request, $loggedIn->isTypeDeveloper());
+
+        $this->applySearch($searchForm, $qb);
+
         return $this->render('@OHMediaSecurity/user/user_index.html.twig', [
             'pagination' => $paginator->paginate($qb, 20),
             'new_user' => new User(),
@@ -62,7 +73,92 @@ class UserController extends AbstractController
                 'delete' => UserVoter::DELETE,
                 'edit' => UserVoter::EDIT,
             ],
+            'search_form' => $searchForm,
         ]);
+    }
+
+    private function getSearchForm(Request $request, bool $showDeveloperType): FormInterface
+    {
+        $formBuilder = $this->container->get('form.factory')
+            ->createNamedBuilder('', FormType::class, null, [
+                'csrf_protection' => false,
+            ]);
+
+        $formBuilder->setMethod('GET');
+
+        $formBuilder->add('search', TextType::class, [
+            'required' => false,
+            'attr' => [
+                'placeholder' => 'User email/name',
+            ],
+        ]);
+
+        $typeChoices = [];
+
+        if ($showDeveloperType) {
+            $typeChoices['Developer'] = User::TYPE_DEVELOPER;
+        }
+
+        $typeChoices['Super Admin'] = User::TYPE_SUPER;
+        $typeChoices['Admin'] = User::TYPE_ADMIN;
+
+        $formBuilder->add('type', ChoiceType::class, [
+            'required' => false,
+            'placeholder' => 'Any',
+            'choices' => $typeChoices,
+        ]);
+
+        $formBuilder->add('enabled', ChoiceType::class, [
+            'required' => false,
+            'choices' => [
+                'Yes' => true,
+                'No' => false,
+            ],
+            'placeholder' => 'Any',
+            'data' => true,
+        ]);
+
+        $form = $formBuilder->getForm();
+
+        $form->handleRequest($request);
+
+        return $form;
+    }
+
+    private function applySearch(FormInterface $form, QueryBuilder $qb): void
+    {
+        $search = $form->get('search')->getData();
+
+        if ($search) {
+            $searchFields = [
+                'u.email',
+                'u.first_name',
+                'u.last_name',
+            ];
+
+            $searchLikes = [];
+            foreach ($searchFields as $searchField) {
+                $searchLikes[] = "$searchField LIKE :search";
+            }
+
+            $qb->andWhere('('.implode(' OR ', $searchLikes).')')
+                ->setParameter('search', '%'.$search.'%');
+        }
+
+        $type = $form->get('type')->getData();
+
+        if ($type) {
+            $qb->andWhere('u.type = :type');
+            $qb->setParameter('type', $type);
+        }
+
+        $enabled = $form->get('enabled')->getData();
+
+        if (true === $enabled) {
+            $qb->andWhere('u.enabled = 1');
+        } elseif (false === $enabled) {
+            $qb->andWhere('(u.enabled = 0 OR u.enabled IS NULL)');
+        }
     }
 
     #[Route('/user/create', name: 'user_create', methods: ['GET', 'POST'])]
