c9303b64fde4721ea187f067437885a33847bf96
Justin Hoffman <justin@ohmedia.ca>
2026-01-13 16:42:07 -0600

    ditch alias

--- src/DependencyInjection/Compiler/EntityChoicePass.php
+++ src/DependencyInjection/Compiler/EntityChoicePass.php
@@ -2,6 +2,7 @@
 
 namespace OHMedia\SecurityBundle\DependencyInjection\Compiler;
 
+use OHMedia\SecurityBundle\Service\EntityChoiceManager;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Reference;
@@ -11,11 +12,11 @@ class EntityChoicePass implements CompilerPassInterface
     public function process(ContainerBuilder $container): void
     {
         // always first check if the primary service is defined
-        if (!$container->has('oh_media_security.entity_choice_manager')) {
+        if (!$container->has(EntityChoiceManager::class)) {
             return;
         }
 
-        $definition = $container->findDefinition('oh_media_security.entity_choice_manager');
+        $definition = $container->findDefinition(EntityChoiceManager::class);
 
         $tagged = $container->findTaggedServiceIds('oh_media_security.entity_choice');
 
